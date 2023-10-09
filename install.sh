934cba532e5cf807f7d8b8e922179b2eac4038d6
Dhruv <dhruvmistry2000@gmail.com>
2023-10-09 23:22:47 +0530

    Remove nitrogen

--- install.sh
+++ install.sh
@@ -28,7 +28,7 @@ mv user-dirs.dirs /home/$username/.config
 chown -R $username:$username /home/$username
 
 # Installing Essential Programs 
-nala install neofetch kitty rofi picom thunar nitrogen lxpolkit x11-xserver-utils unzip wget pulseaudio pavucontrol build-essential libx11-dev libxft-dev libxinerama-dev -y
+nala install neofetch kitty rofi picom thunar lxpolkit x11-xserver-utils unzip wget pulseaudio pavucontrol build-essential libx11-dev libxft-dev libxinerama-dev -y
 # Installing Other less important Programs
 nala install neofetch flameshot psmisc mangohud lxappearance papirus-icon-theme lxappearance fonts-noto-color-emoji lightdm -y
 
