0a43771a9b6e7eb01613a56deb0de4ed88f3e71c
Dhruv <dhruvmistry2000@gmail.com>
2023-10-15 12:06:05 +0530

    Brave Remove

--- install.sh
+++ install.sh
@@ -58,13 +58,6 @@ cd Nordzy-cursors
 cd $builddir
 rm -rf Nordzy-cursors
 
-# Install brave-browser
-nala install apt-transport-https curl -y
-curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | tee /etc/apt/sources.list.d/brave-browser-release.list
-nala update
-nala install brave-browser -y
-
 
 # Enable graphical login and change target from CLI to GUI
 systemctl enable lightdm
